e44085dc0c6c8e05de817affaa9c392f54ed868e
SaiChand440 <59303987+SaiChand440@users.noreply.github.com>
2021-05-23 17:27:46 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Material(
+          child: Text("Welcome to flutter application"),
+        ),
+      ),
+    );
+  }
+}
